85e31ece76caea2e69a9c46bffe260f4211dc281
Thonxyzz404 <luubotakluukren432@gmail.com>
2024-12-15 19:19:56 +0700

    Add files via upload

---
+++ hack_wa.py
@@ -0,0 +1,48 @@
+############################ SCRIPT HACK WA, BAJAK WA BY THONXYZZ404 GABTENGG HENGHKERR TEZETYEEE ############################
+
+import os, json, random, time
+from time import sleep
+
+def generate_otp():
+    otp = random.randint(100000, 999999)
+    return otp
+
+if os.name == "posix":
+  os.system("clear")
+if os.name == "nt":
+  os.system("clear")
+
+banner = """\033[93m
+                             \033[0mH      A      C      K      -      W      A\033[93m
+                       ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ██╗    ██╗ █████╗
+                       ██║  ██║██╔══██╗██╔════╝██║ ██╔╝     ██║    ██║██╔══██╗
+                       ███████║███████║██║     █████╔╝█████╗██║ █╗ ██║███████║
+                       ██╔══██║██╔══██║██║     ██╔═██╗╚════╝██║███╗██║██╔══██║
+                       ██║  ██║██║  ██║╚██████╗██║  ██╗     ╚███╔███╔╝██║  ██║
+                       ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚══╝╚══╝ ╚═╝  ╚═╝
+
+                       \033[92m———————————————————— \033[0mHack WhatsApp \033[92m————————————————————
+                                             \033[0mBy Thonxyzz404
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————"""
+
+def hack_wa():
+    print(banner)
+    print ()
+    sleep(0.50)
+    hek = input(f"\033[91m[\033[94mUser@localhost\033[91m]\033[0;37m Enter WhatsApp Target:\033[1;97m ")
+    print ()
+    sleep(2)
+    print(f"\033[1;91m[\033[92mINFO\033[91m]\033[0m Succsess Get OTP WhatsApp Code from \033[94m{hek}")
+    sleep(2)
+    print ()
+    otp_code = generate_otp()
+    sleep(1)
+    print("\033[1;91m[\033[92m>\033[91m]\033[0m OTP Code:\033[0;34m", otp_code)
+    wait = input("")
+    print ()
+
+### start hack
+hack_wa()
+
+############################ script ini di buat oleh Thonxyzz404 gamteng henhker tezetye anonimos ############################
